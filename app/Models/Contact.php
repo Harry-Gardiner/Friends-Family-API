c733fe8a162c8163180422e3ba1a9238ddc00640
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-04 09:59:17 +0000

    protected fillable added Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $fillable = [
+        'full_name',
+        'email',
+        'phone',
+        'date_of_birth',
+    ];
+    
     use HasFactory;
 }
